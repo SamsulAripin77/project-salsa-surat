0e0f914623ba5464746a9fb562a127a48a31bda2
SamsulAripin77 <arifins192@gmail.com>
2021-07-17 19:04:59 +0700

    add logo dishub

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.app')
 @section('content')
-<div class="row p-3 mb-5 justify-content-center text-center">
+<div class="row p-3 mb-2 justify-content-center text-center">
     <div class="col-lg-12 col-md-12">
-        <img src="https://homepages.cae.wisc.edu/~ece533/images/airplane.png" alt="" width="100" height="100">
+        <img src="{{ asset('img/logo.png') }}" alt="" width="220" height="150">
         <h3 class="text-center">Sistem Informasi Pengelolaan Surat</h3>
         <h3 class="text-center">Dinas Perhubungan Kabupaten Sukabumi</h3>
     </div>
